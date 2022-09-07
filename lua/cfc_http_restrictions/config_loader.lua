ec69d9a6bd89376a5c6f57a61f834b9854e32466
Redox <mail@redox.to>
2022-09-07 03:03:22 +0200

    Style, shadow bindings

--- lua/cfc_http_restrictions/config_loader.lua
+++ lua/cfc_http_restrictions/config_loader.lua
@@ -1,8 +1,8 @@
 CFCHTTP = CFCHTTP or {}
-CFCHTTP.config = include("default_config.lua")
+CFCHTTP.config = include( "default_config.lua" )
 
 function CFCHTTP.LoadConfigs()
-    CFCHTTP.config = include("default_config.lua")
+    CFCHTTP.config = include( "default_config.lua" )
     CFCHTTP.loadLuaConfigs()
 
     if CLIENT then
@@ -14,41 +14,41 @@ function CFCHTTP.LoadConfigs()
 end
 
 -- LoadLuaConfigs loads the default config and then any lua files in the cfc_http_restrictions/configs directory
-function CFCHTTP.loadLuaConfigs() 
-    local files = file.Find("cfc_http_restrictions/configs/*.lua", "LUA")
-    for _, file in pairs(files) do
-        AddCSLuaFile("cfc_http_restrictions/configs/" .. file)
-        local newConfig = include("cfc_http_restrictions/configs/" .. file)
-        CFCHTTP.config = CFCHTTP.mergeConfigs(CFCHTTP.config, newConfig)
+function CFCHTTP.loadLuaConfigs()
+    local files = file.Find( "cfc_http_restrictions/configs/*.lua", "LUA" )
+    for _, fil in pairs( files ) do
+        AddCSLuaFile( "cfc_http_restrictions/configs/" .. fil )
+        local newConfig = include( "cfc_http_restrictions/configs/" .. fil )
+        CFCHTTP.config = CFCHTTP.mergeConfigs( CFCHTTP.config, newConfig )
     end
 end
 
-function CFCHTTP.mergeConfigs(old, new)
+function CFCHTTP.mergeConfigs( old, new )
     if new.version == "1" then
         if new.wrapHTMLPanels ~= nil then old.wrapHTMLPanels = new.wrapHTMLPanels end
         if new.defaultOptions ~= nil then old.defaultOptions = new.defaultOptions end
         if new.defaultAssetURIOption ~= nil then old.defaultAssetURIOption = new.defaultAssetURIOption end
-        
-        for domain, options in pairs(new.addresses) do
+
+        for domain, options in pairs( new.addresses ) do
             local currentOptions = old.addresses[domain]
             if currentOptions and currentOptions.permanent then
-                print("[CFC HTTP Restrictions] Skipping " .. domain .. " because it is permanent")
+                print( "[CFC HTTP Restrictions] Skipping " .. domain .. " because it is permanent" )
             else
                 old.addresses[domain] = options
             end
         end
     else
-        ErrorNoHalt("[CFC HTTP Restrictions] Invalid config version: " .. tostring(new.version))
+        ErrorNoHalt( "[CFC HTTP Restrictions] Invalid config version: " .. tostring( new.version ) )
     end
 
     return old
 end
 
-function CFCHTTP.copyConfig(cfg)
-    return util.JSONToTable(util.TableToJSON(cfg))
+function CFCHTTP.copyConfig( cfg )
+    return util.JSONToTable( util.TableToJSON( cfg ) )
 end
 
-function CFCHTTP.saveFileConfig(config)
+function CFCHTTP.saveFileConfig( config )
     file.Write( "cfc_cl_http_whitelist_config.json", util.TableToJSON( config, true ) )
 
     notification.AddLegacy( "Saved http whitelist", NOTIFY_GENERIC, 5 )
