e0b8ac59a78c04cd42ebd72c64124b6ea12ed57a
Pierce <pierce@vulpes.dev>
2023-07-21 15:24:51 -0400

    add filetypes.lua

---
+++ lua/cfc_http_restrictions/client/filetypes.lua
@@ -0,0 +1,36 @@
+CFCHTTP.FileTypes = CFCHTTP.FIleTypes or {}
+
+local files, _ = file.Find( "cfc_http_restrictions/client/filetypes/*.lua", "LUA" )
+for _, f in pairs( files ) do
+    include( "cfc_http_restrictions/client/filetypes/" .. f )
+    AddCSLuaFile( "cfc_http_restrictions/client/filetypes/" .. f )
+end
+
+---@param data string
+---@return table|nil
+function CFCHTTP.getFileType( data )
+    for _, f in pairs( CFCHTTP.FileTypes ) do
+        if f.IsFileData( data ) then
+            return f
+        end
+    end
+    return nil
+end
+
+---@param url string
+---@param callback fun( urls: string[], err: string|nil)
+function CFCHTTP.GetFileDataURLS( url, callback )
+    http.Fetch( url, function( body )
+        local filetype = CFCHTTP.getFileType( body )
+        if filetype then
+            local urls, err = filetype.GetURLSFromData( body )
+            if err then
+                callback( {}, err )
+            else
+                callback( urls, nil )
+            end
+        else
+            callback( {}, nil )
+        end
+    end )
+end
