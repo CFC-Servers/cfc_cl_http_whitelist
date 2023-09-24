f2b8ec7fd074c6692f4347d2a7e102d643a343b0
Pierce <pierce@vulpes.dev>
2023-09-23 20:21:36 -0400

    remove debug print

--- lua/cfc_http_restrictions/wraps/http.lua
+++ lua/cfc_http_restrictions/wraps/http.lua
@@ -3,7 +3,6 @@ local function wrapHTTP()
     print( "HTTP wrapped, original function at '_G._HTTP'" )
 
     HTTP = function( req )
-        print( "HTTP request to " .. req.url )
         local options = CFCHTTP.GetOptionsForURL( req.url )
         local isAllowed = options and options.allowed
         local noisy = options and options.noisy
